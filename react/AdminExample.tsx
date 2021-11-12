cca6dfd07f8d5171af84cb86c0c06ddea24bc0a0
kevin <kevinchevallier@gmail.com>
2021-11-12 10:30:38 -0300

    Add Datagrid

--- react/AdminExample.tsx
+++ react/AdminExample.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import { useRuntime } from 'vtex.render-runtime'
+// import { useRuntime } from 'vtex.render-runtime'
 import { defineMessages, FormattedMessage } from 'react-intl'
 import {
   createSystem,
   PageHeader,
   PageTitle,
-  List,
+  DataGrid,
+  useDataGridState,
+  PageActions,
   Button,
+  DataView,
+  DataViewControls,
+  Search,
+  useDataViewState,
+  useSearchState,
 } from '@vtex/admin-ui'
+
+import { items } from './data'
 
 const [ThemeProvider] = createSystem({
   key: 'admin-ui-example',
@@ -26,7 +35,44 @@ const messages = defineMessages({
 })
 
 function AdminExample() {
-  const { navigate } = useRuntime()
+  // const { navigate } = useRuntime()
+
+  const view = useDataViewState()
+  const search = useSearchState()
+
+  const searchedItems = React.useMemo(() => {
+    return items.filter((item) =>
+      item.productName.toLowerCase().startsWith(
+        // use the search debounced value to
+        // filter the collection
+        search.debouncedValue.toLocaleLowerCase()
+      )
+    )
+  }, [search])
+
+  const grid = useDataGridState({
+    view,
+    columns: [
+      {
+        id: 'productName',
+        header: 'Product Name',
+      },
+      {
+        id: 'inStock',
+        header: 'In Stock',
+      },
+      {
+        id: 'skus',
+        header: 'SKUs',
+      },
+      {
+        id: 'price',
+        header: 'Price',
+      },
+    ],
+    items: searchedItems,
+    length: 5,
+  })
 
   return (
     <ThemeProvider>
@@ -34,38 +80,21 @@ function AdminExample() {
         <PageTitle>
           <FormattedMessage {...messages.title} />
         </PageTitle>
+
+        <PageActions>
+          <Button variant="secondary">Edit</Button>
+          <Button>Create</Button>
+        </PageActions>
       </PageHeader>
 
-      <List>
-        <List.Item>
-          <FormattedMessage {...messages.itemId} values={{ refId: '6745' }} />{' '}
-          <Button
-            size="small"
-            onClick={() => {
-              navigate({
-                page: 'admin.app.example-detail',
-                params: { id: 6745 },
-              })
-            }}
-          >
-            <FormattedMessage {...messages.buttonLabel} />
-          </Button>
-        </List.Item>
-        <List.Item>
-          <FormattedMessage {...messages.itemId} values={{ refId: '1987' }} />{' '}
-          <Button
-            size="small"
-            onClick={() => {
-              navigate({
-                page: 'admin.app.example-detail',
-                params: { id: 1987 },
-              })
-            }}
-          >
-            <FormattedMessage {...messages.buttonLabel} />
-          </Button>
-        </List.Item>
-      </List>
+      <div style={{ padding: '0 4rem' }}>
+        <DataView state={view}>
+          <DataViewControls>
+            <Search id="search" placeholder="Search" state={search} />
+          </DataViewControls>
+          <DataGrid state={grid} />
+        </DataView>
+      </div>
     </ThemeProvider>
   )
 }
---
+++ react/data.ts
@@ -0,0 +1,44 @@
+export const items = [
+  {
+    id: 1,
+    productName: 'Banana',
+    inStock: 380,
+    skus: 0,
+    price: 80,
+  },
+  {
+    id: 2,
+    productName: 'Lemon',
+    inStock: 380,
+    skus: 26,
+    price: 500,
+  },
+  {
+    id: 3,
+    productName: 'Tomato',
+    inStock: 380,
+    skus: 25,
+    price: 100,
+  },
+  {
+    id: 4,
+    productName: 'Ananas',
+    inStock: 380,
+    skus: 0,
+    price: 80,
+  },
+  {
+    id: 5,
+    productName: 'Citron',
+    inStock: 380,
+    skus: 26,
+    price: 500,
+  },
+  {
+    id: 6,
+    productName: 'Bui',
+    inStock: 380,
+    skus: 25,
+    price: 100,
+  },
+]
