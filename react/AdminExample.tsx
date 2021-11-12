647daf7ecf79c7831fca4f697bdfe7c8186dc9a7
kevin <kevinchevallier@gmail.com>
2021-11-12 11:56:08 -0300

    Add search bar & pagination

--- react/AdminExample.tsx
+++ react/AdminExample.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { useRuntime } from 'vtex.render-runtime'
+import { useRuntime } from 'vtex.render-runtime'
 import { defineMessages, FormattedMessage } from 'react-intl'
 import {
   createSystem,
@@ -14,9 +14,24 @@ import {
   Search,
   useDataViewState,
   useSearchState,
+  usePaginationState,
+  FlexSpacer,
+  Pagination,
 } from '@vtex/admin-ui'
+import faker from 'faker'
 
-import { items } from './data'
+interface Item {
+  id: number
+  inStock: number
+  lastSale: string
+  price: string
+  productName: string
+  productAdjective: string
+  skus: number
+}
+
+const NUMBER_OF_ITEMS = 45
+const ITEMS_PER_PAGE = 10
 
 const [ThemeProvider] = createSystem({
   key: 'admin-ui-example',
@@ -34,21 +49,42 @@ const messages = defineMessages({
   },
 })
 
+const items = Array(NUMBER_OF_ITEMS)
+  .fill(NUMBER_OF_ITEMS)
+  .map(() => {
+    return {
+      id: faker.unique(() => faker.datatype.number(1000)),
+      productName: faker.commerce.productName(),
+      productAdjective: faker.commerce.productAdjective(),
+      lastSale: faker.date.past().toDateString(),
+      price: faker.commerce.price(),
+      inStock: faker.datatype.number(1000),
+      skus: faker.datatype.number(10000),
+    }
+  })
+
 function AdminExample() {
-  // const { navigate } = useRuntime()
+  const { navigate } = useRuntime()
 
   const view = useDataViewState()
   const search = useSearchState()
+  const pagination = usePaginationState({
+    pageSize: ITEMS_PER_PAGE,
+    total: NUMBER_OF_ITEMS,
+  })
 
   const searchedItems = React.useMemo(() => {
-    return items.filter((item) =>
-      item.productName.toLowerCase().startsWith(
-        // use the search debounced value to
-        // filter the collection
-        search.debouncedValue.toLocaleLowerCase()
-      )
-    )
+    return items.filter((item) => {
+      const searchLowerCase = search.debouncedValue.toLocaleLowerCase()
+
+      return item.productName.toLowerCase().includes(searchLowerCase)
+    })
   }, [search])
+
+  React.useEffect(() => {
+    pagination.paginate({ type: 'setTotal', total: searchedItems.length })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchedItems.length])
 
   const grid = useDataGridState({
     view,
@@ -56,6 +92,14 @@ function AdminExample() {
       {
         id: 'productName',
         header: 'Product Name',
+      },
+      {
+        id: 'productAdjective',
+        header: 'Adjective',
+      },
+      {
+        id: 'lastSale',
+        header: 'Last Sale',
       },
       {
         id: 'inStock',
@@ -68,10 +112,21 @@ function AdminExample() {
       {
         id: 'price',
         header: 'Price',
+        resolver: {
+          type: 'currency',
+          locale: 'en-US',
+          currency: 'USD',
+        },
       },
     ],
-    items: searchedItems,
-    length: 5,
+    items: searchedItems.slice(pagination.range[0] - 1, pagination.range[1]),
+    length: ITEMS_PER_PAGE,
+    onRowClick: (el: Item) => {
+      navigate({
+        page: 'admin.app.example-detail',
+        params: { ...el },
+      })
+    },
   })
 
   return (
@@ -91,6 +146,14 @@ function AdminExample() {
         <DataView state={view}>
           <DataViewControls>
             <Search id="search" placeholder="Search" state={search} />
+            <FlexSpacer />
+            <Pagination
+              state={pagination}
+              preposition="of"
+              subject="results"
+              prevLabel="Previous"
+              nextLabel="Next"
+            />
           </DataViewControls>
           <DataGrid state={grid} />
         </DataView>
